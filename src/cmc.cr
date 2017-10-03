bcfb4d34d8d8d485963292c8f14ee20840169e39
Croccifixio <aizenkrosis@hotmail.com>
2017-10-03 23:00:44 +0200

    remove commented code, enhance useful comments

--- src/cmc.cr
+++ src/cmc.cr
@@ -2,12 +2,6 @@ require "./cmc/*"
 require "http/client"
 require "option_parser"
 
-  # def api_check
-  #   HTTP::Client.new('api.coinmarketcap.com', 443, true) do |client|
-  #     response = client.post_from('/v1/ticker')
-  #   end
-  # end
-
 module CMC
   def call
     fullInfo = false
@@ -47,11 +41,6 @@ module CMC
       end
     end
 
-    # response = HTTP::Client.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
-    # header_status = "Status: #{response.status_code}"
-    # header_time = "Last updated: #{minutes_since(tokens.last.token.last_updated)} minutes ago"
-
-    # print_header(header_status, header_time, 60_i8)
     print_table(headings, tokens)
   end
 
@@ -66,17 +55,25 @@ end
 
 API.call
 
-  # id": "bitcoin",
-  # "name": "Bitcoin",
-  # "symbol": "BTC",
-  # "rank": "1",
-  # "price_usd": "4182.83",
-  # "price_btc": "1.0",
-  # "24h_volume_usd": "1388010000.0",
-  # "market_cap_usd": "69413018143.0",
-  # "available_supply": "16594750.0",
-  # "total_supply": "16594750.0",
-  # "percent_change_1h": "-0.01",
-  # "percent_change_24h": "0.4",
-  # "percent_change_7d": "15.91",
-  # "last_updated": "1506720851"
+
+######################################
+# EXAMPLE JSON RESPONSE FROM THE API #
+######################################
+# [
+#   {
+#     id": "bitcoin",
+#     "name": "Bitcoin",
+#     "symbol": "BTC",
+#     "rank": "1",
+#     "price_usd": "4182.83",
+#     "price_btc": "1.0",
+#     "24h_volume_usd": "1388010000.0",
+#     "market_cap_usd": "69413018143.0",
+#     "available_supply": "16594750.0",
+#     "total_supply": "16594750.0",
+#     "percent_change_1h": "-0.01",
+#     "percent_change_24h": "0.4",
+#     "percent_change_7d": "15.91",
+#     "last_updated": "1506720851"
+#   }
+# ]
